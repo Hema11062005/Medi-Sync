f1cc613f3ed6f417492bac5cf171d70e315ffacd
Hema <hemalathar11062005@gmail.com>
2026-08-12 16:22:00 +0530

    Add doctor delete and update routes

--- server/src/modules/doctors/doctorRoute.js
+++ server/src/modules/doctors/doctorRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {protect} = require("../../middleware/authMiddleware");
+const { protect } = require("../../middleware/authMiddleware");
 
 const {
-    createDoctor,
-    getDoctors
+  createDoctor,
+  getDoctors,
+  updateDoctor,
+  deleteDoctor,
 } = require("./doctorController");
 
 const router = express.Router();
@@ -13,5 +15,11 @@ router.post("/", protect, createDoctor);
 
 // Get all doctors
 router.get("/", protect, getDoctors);
+
+// Update doctor
+router.put("/:id", protect, updateDoctor);
+
+// Delete doctor
+router.delete("/:id", protect, deleteDoctor);
 
 module.exports = router;
